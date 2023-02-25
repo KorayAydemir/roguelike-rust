2fea2bf7e1bd39735eb03a8c5e499aaaaab5b346
Koray Aydemir <korayaydemir276@gmail.com>
2023-02-25 22:18:44 +0300

    fix: cant go through walls now

--- src/main.rs
+++ src/main.rs
@@ -75,9 +75,14 @@ fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
 
+    let map = ecs.fetch::<Vec<TileType>>();
+
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
