07937cb271a55a0088c128b47524d70e6044091b
Koray Aydemir <korayaydemir276@gmail.com>
2023-02-25 21:35:11 +0300

    added walls but doesnt work

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use bracket_lib::terminal::{
-    to_cp437, BTerm, BTermBuilder, FontCharType, GameState, VirtualKeyCode, BLACK, RED, RGB, YELLOW,
+use bracket_lib::{
+    random::RandomNumberGenerator,
+    terminal::{
+        self, to_cp437, BTerm, BTermBuilder, FontCharType, GameState, VirtualKeyCode, BLACK, RED,
+        RGB, YELLOW,
+    },
 };
 
 use specs::prelude::*;
@@ -8,6 +12,82 @@ use std::cmp::{max, min};
 
 struct State {
     ecs: World,
+}
+
+#[derive(PartialEq, Clone, Copy)]
+enum TileType {
+    Wall,
+    Floor,
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80 * 50];
+
+    // Make the boundaries walls
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    // Randomly splat a bunch of walls.
+    // First, obtain the thread-local RNG:
+    let mut rng = RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+fn draw_map(map: &[TileType], ctx: &mut BTerm) {
+    let mut y = 0;
+    let mut x = 0;
+    for tile in map.iter() {
+        // Render a tile depending upon the tile type
+        match tile {
+            TileType::Floor => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.5, 0.5, 0.5),
+                    RGB::from_f32(0., 0., 0.),
+                    terminal::to_cp437('.'),
+                );
+            }
+            TileType::Wall => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.0, 1.0, 0.0),
+                    RGB::from_f32(0., 0., 0.),
+                    terminal::to_cp437('#'),
+                );
+            }
+        }
+    }
+
+    // Move the coords
+    x += 1;
+    if x > 79 {
+        x = 0;
+        y += 1;
+    }
 }
 
 #[derive(Component, Debug)]
@@ -58,6 +138,9 @@ impl GameState for State {
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
+
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
     }
 }
 
@@ -105,6 +188,7 @@ fn main() {
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
+    gs.ecs.insert(new_map());
 
     gs.ecs
         .create_entity()
